7c9f67aa298926feeab483ea4155cf44f04d4750
RAUSHAN KUMAR <44130364+iiitu17208@users.noreply.github.com>
2019-09-24 17:46:27 +0530

    Add files via upload

---
+++ assignment -3 SOLUTION.py
@@ -0,0 +1,134 @@
+'''
+#1.>>
+print('The first 10 natural number is :-')
+x=1
+while x<=10:
+    print(x)
+    x+=1
+'''
+
+'''
+#2.>>
+print('The first 10 natural number in reverse order is :-')
+x=10
+while x>=1:
+    print(x)
+    x-=1
+'''
+
+'''
+#3.>>
+print('The first 10 ODD natural number is :-')
+x=1
+while x<=20:
+    print(x)
+    x+=2
+'''
+
+'''
+#4.>>
+print('The first 10 ODD natural number is :-')
+x=2
+while x<=20:
+    print(x)
+    x+=2
+'''
+
+'''
+#5.>>
+N=int(input('Enter the value of N :'))
+print('The first %d natural number is - ')
+x=1
+while x<=N:
+    print(x)
+    x+=1
+'''
+
+'''
+#6.>>
+N=int(input('Enter the value of N :'))
+print('The first %d natural number is - ')
+x=N
+while x>=1:
+    print(x)
+    x-=1
+'''
+
+'''
+#7.>>
+N=int(input('Enter the value of N :'))
+SUM=0
+x=1
+while x<=N:
+    SUM+=x
+    x+=1
+print('The sum of first %d Ntural Number is %d'%(N,SUM))    
+'''
+
+'''
+#8.>>
+N=int(input('Enter the value of N :'))
+SUM=0
+x=1
+while x<=2*N:
+    SUM+=x
+    x+=2
+print('The sum of first %d Ntural Number is %d'%(N,SUM)) 
+'''
+
+'''
+#9.>>
+N=int(input('Enter the number :'))
+x=N
+fact=1
+while N>=1:
+    fact*=N
+    N-=1
+print('The factorial of %d is %d'%(x,fact))    
+'''
+
+'''
+#10.>>
+N=int(input('Enter the value of N :'))
+x=1
+prod=1
+while x<=2*N:
+    prod*=x
+    x+=2
+print('the product of first %d ODD natural number is %d'%(N,prod))    
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
